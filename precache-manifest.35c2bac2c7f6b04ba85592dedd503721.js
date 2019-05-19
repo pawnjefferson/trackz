96e2abd9ef193a4018e4d055c60c3928efd1b414
pawnjefferson <pawnjefferson@gmail.com>
2019-05-19 19:52:09 -0400

    Updates

---
+++ precache-manifest.35c2bac2c7f6b04ba85592dedd503721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34b557e72eb2517f1966ace7570aad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660af80c7466eb92db91",
+    "url": "/static/css/main.93cb4068.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "660af80c7466eb92db91",
+    "url": "/static/js/main.26e1e0cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c62b39cb1107a18802e00be3a6e911bc",
+    "url": "/static/media/dol.c62b39cb.jpg"
+  }
+]);
